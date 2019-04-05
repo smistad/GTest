bf020642a9d4b34145f281b044959a9cbe1ceeee
Melroy van den Berg <webmaster1989@gmail.com>
2019-04-05 17:22:13 +0200

    Update tests.cpp
    Remove main

--- tests.cpp
+++ tests.cpp
@@ -12,8 +12,3 @@ TEST(SquareRootTest, NegativeNos) {
     ASSERT_EQ(-1.0, squareRoot(-15.0));
     ASSERT_EQ(-1.0, squareRoot(-0.2));
 }
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
